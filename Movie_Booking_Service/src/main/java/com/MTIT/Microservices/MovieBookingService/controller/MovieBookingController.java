56675d5443afb490d44d07245d9e11cdfdc4695d
himasha <himasha.mandakini@gmail.com>
2022-06-11 20:09:03 +0530

    Controller class Implementation

--- Movie_Booking_Service/src/main/java/com/MTIT/Microservices/MovieBookingService/controller/MovieBookingController.java
+++ Movie_Booking_Service/src/main/java/com/MTIT/Microservices/MovieBookingService/controller/MovieBookingController.java
@@ -2,24 +2,48 @@ package com.MTIT.Microservices.MovieBookingService.controller;
 
 
 import com.MTIT.Microservices.MovieBookingService.models.MovieBooking;
-import com.MTIT.Microservices.MovieBookingService.service.MovieBookingService;
+import com.MTIT.Microservices.MovieBookingService.service.MovieBoookingServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
+
 @RestController
-@RequestMapping("/book")
-public class MovieBookingController{
+@RequestMapping("/bookings")
+public class MovieBookingController {
 
     @Autowired
-    private MovieBookingService bookingService;
+    private MovieBoookingServiceImpl bookingService;
 
-    @GetMapping("/bookedmovies")
+    @PostMapping("/book")
+    public String add(@RequestBody MovieBooking movieBooking) {
+        bookingService.bookMovie(movieBooking);
+        return "Saved...";
+    }
+
+    @GetMapping("/view")
     public List<MovieBooking> getALlBookedMovies() {
         return bookingService.listBookedMovies();
+    }
+
+    @PutMapping("/book/{bookingId}")
+    public String update(@RequestBody MovieBooking movieBooking, @PathVariable Integer bookingId) {
+        MovieBooking getExistingBooking = bookingService.getBookedMovieDetails(bookingId);
+        if(getExistingBooking.getBookingId() == movieBooking.getBookingId()){
+            bookingService.bookMovie(movieBooking);
+            return movieBooking.toString() + "Updated Successfully";
+        }
+        else{
+            return "Selected Booking does not exist";
+        }
+    }
+
+    @DeleteMapping("/view/{bookingId}")
+    public String cancelBooking(@PathVariable Integer bookingId){
+        bookingService.deleteBooking(bookingId);
+        return "Booking " +  bookingId + " Cancelled Successfully";
     }
 
 }
