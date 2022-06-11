d0c95f94d4463f9dd08818d737a6091d50595f54
sandamaligeethika <sgeethika98@gmail.com>
2022-06-11 22:57:30 +0530

    Added inter dependency communication from movie service

--- Movie_Rating_Service/rating-service/src/main/java/com/MTIT/microservice/movie/ratingservice/controller/MovieRatingController.java
+++ Movie_Rating_Service/rating-service/src/main/java/com/MTIT/microservice/movie/ratingservice/controller/MovieRatingController.java
@@ -2,6 +2,7 @@ package com.MTIT.microservice.movie.ratingservice.controller;
 
 import com.MTIT.microservice.movie.ratingservice.exception.ResourceNotFoundException;
 import com.MTIT.microservice.movie.ratingservice.model.MovieRating;
+import com.MTIT.microservice.movie.ratingservice.model.MovieRatingDto;
 import com.MTIT.microservice.movie.ratingservice.service.RatingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -17,10 +18,10 @@ public class MovieRatingController {
     public MovieRating createMovie(@RequestBody MovieRating movieRating) {
         return ratingService.save(movieRating);
     }
+
     @GetMapping("/movie/{movieId}")
-    public ResponseEntity<MovieRating> getMovieRating(@PathVariable long movieId) {
-        MovieRating movie = ratingService.getMovieRatingForMovieId(movieId)
-                .orElseThrow(() -> new ResourceNotFoundException("MovieRating not found for movie with id : " + movieId));
+    public ResponseEntity<MovieRatingDto> getMovieRating(@PathVariable long movieId) {
+        MovieRatingDto movie = ratingService.getMovieRatingForMovieId(movieId);
 
         return ResponseEntity.ok(movie);
     }
---
+++ Movie_Rating_Service/rating-service/src/main/java/com/MTIT/microservice/movie/ratingservice/model/Movie.java
@@ -0,0 +1,19 @@
+package com.MTIT.microservice.movie.ratingservice.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Movie {
+    private long movieId;
+
+    private String movieTitle;
+
+    private String movieType;
+
+    private int year;
+
+}
